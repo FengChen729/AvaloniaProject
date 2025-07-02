aa3c7d66b7c04a39e52117f64032a1a80fd872b9
FengChen <15989060895@163.com>
2025-07-02 11:02:30 +0800

    Application2 is implementation of the table

---
+++ AvaloniaApplication2/Models/Person.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace AvaloniaApplication2.Models
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    OnPropertyChanged(nameof(FirstName));
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    OnPropertyChanged(nameof(LastName));
+                }
+            }
+        }
+        
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ AvaloniaApplication2/ViewModels/MainWindowModels.cs
@@ -0,0 +1,35 @@
+﻿using AvaloniaApplication2.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using AvaloniaApplication2.Commands;
+
+namespace AvaloniaApplication2.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Person> People { get; }
+ 
+        public ICommand AddPersonCommand { get; }
+        public ICommand RemovePersonCommand { get; }
+        
+        public MainWindowViewModel()
+        {
+            People = new ObservableCollection<Person>
+            {
+                new Person("Neil", "Armstrong"),
+                new Person("Buzz", "Lightyear"),
+                new Person("James", "Kirk")
+            };
+
+            AddPersonCommand = new RelayCommand(AddPerson);
+            RemovePersonCommand = new RelayCommand(RemovePerson);
+        }
+        
+        private void AddPerson() => People.Add(new Person("New", "People"));
+
+        private void RemovePerson()
+        {
+            if (People.Count > 0) People.RemoveAt(People.Count - 1);
+        }
+    }
+}
