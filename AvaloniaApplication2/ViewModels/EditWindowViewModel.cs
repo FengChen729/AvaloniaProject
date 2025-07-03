d6a2da90a6ee504183fd7d001c4537d066dbebcc
FengChen <15989060895@163.com>
2025-07-03 10:37:35 +0800

    终于实现从MainWindow到EditWindow的跳转功能了

---
+++ AvaloniaApplication2/ViewModels/EditWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AvaloniaApplication2.ViewModels
+{
+    public class EditWindowViewModel : INotifyPropertyChanged
+    {
+        private string _displayText;
+        
+        public string DisplayText
+        {
+            get => _displayText;
+            set
+            {
+                if (_displayText != value)
+                {
+                    _displayText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public EditWindowViewModel(string name)
+        {
+            DisplayText = $"This is {name}";
+        }
+    }
+}
---
+++ AvaloniaApplication2/Views/EditWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+
+namespace AvaloniaApplication2.Views
+{
+    public partial class EditWindow : Window
+    {
+        public EditWindow(string name)
+        {
+            InitializeComponent();
+            NameTextBlock.Text = $"This is {name}";
+        }
+    }
+}
